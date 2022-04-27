9a02fadf19fc45362524768a1e2aaeb3f79779d8
nomoney34 <dragostrandafir80@gmail.com>
2022-04-27 03:25:05 +0300

    Finished cron

--- cron.js
+++ cron.js
@@ -9,7 +9,7 @@ const UBI = require("./UBI.json");
 
 const networks = [
     {
-        url: "https://rinkeby.infura.io/v3/",
+        url: process.env.INFURA_KEY,
         chainId: 4,
     },
 
@@ -26,7 +26,7 @@ const networks = [
     }
 ]
 
-const contractAddress = "0xa53c232aE15e67D7c25CecB6f4E4bd8470b7A136";
+const contractAddress = "0x8FA004a40d3E90D89eE5C6B3ac7CBB7B34C1ee28";
 const chains = networks.map((networks) => {
     const provider = new ethers.providers.JsonRpcProvider(networks.url);
     const signer = new ethers.Wallet(process.env.CRON_KEY, provider);
@@ -35,41 +35,122 @@ const chains = networks.map((networks) => {
     return contract;
 });
 
-// Every second the cron checks if owner enabled subscriptions
-cron.schedule("* * * * * *", function listenForSubEnable(){
-    
-    for (const c of chains) {
-        c.on("EmitSubscriptions", (enable) => {
-            console.log(enable);
-            const e = async() => {
-                try {
-                    await c.enableSubscription();
-                }
-                catch(error) {
-                    console.log(error);
-                }
-            };
 
-            e();
+const d = async(c) => {
+    try {
+        await c.disableSubscription();
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+const e = async(c) => {
+    try {
+        await c.enableSubscription();
+    }
+    catch(error) {
+        console.log(error);
+    }
+};
+
+// Every second the cron checks if owner enabled subscriptions
+console.log("Started scheduler.");
+
+var enableListener = cron.schedule("* * * * * *", function listenForSubEnable(){
+    console.log("enable");
+    for(const c of chains) {
+        c.on("EnableSubscriptions", (enable, event) => {
+            for(const x of chains) {
+                e(x);
+            }
+
+            disableListener.start();
+            checkIncome.start();
+            enableListener.stop();
             
         });
+
+        
+
+    }
+
+});
+
+enableListener.start();
+
+
+
+const subscribe = async(c, addr) => {
+    try {
+        await c.getSubscription(addr);
+    } catch (error){
+        console.log(error);
+    }
+}
+
+const unsubscribe = async(c) => {
+    try {
+        await c.stopSubscription(addr);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+var disableListener = cron.schedule("* * * * * *", function listenForSubDisable(){
+    console.log("disable");
+    for(const c of chains) {
+        c.on("DisableSubscriptions", (enable, event) => {
+            for(const x of chains) {
+                d(x);
+            }
+
+            enableListener.start();
+            checkIncome.stop();
+            disableListener.stop();
+            
+        });
+
+        c.on("Subscribe", (addr, event) => {
+            for(const x of chains) {
+                subscribe(x, addr);
+            }
+        });
+
+        c.on("Unsubscribe", (addr, event) => {
+            for(const x of chains) {
+                unsubscribe(x, addr);
+            }
+        });
+    }
+});
+
+disableListener.stop();
+
+
+const check = async(c) => {
+    try {
+        await c.checkIncomes();
+    }
+    catch(error){
+        console.log(error);
+    };
+
+    
+};
+
+// Every 2 minutes (set to 1 for speed) the cron checks contract income.
+// It will distribute to users based on the amount recieved in that very minute
+var checkIncome = cron.schedule("*/1 * * * *", function checkIncomes(){
+    
+    console.log("checked");
+    for(const c of chains){
+        check(c);
     }
 
     
 });
 
-// Every minute the cron checks contract income on each minute.
-cron.schedule("*/1 * * * *", function checkIncomes(){
-    for(const c of chains){
-        const f = async() => {
-            try {
-                await c.checkIncomes();
-            }
-            catch(error){
-                console.log(error);
-            };
+checkIncome.stop();
 
-            f();
-        };
-    }
-});
+// checkIncome.start();
